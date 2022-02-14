e8ae8200e6e41d11951f45d4366b7554dcff1aea
Nandini <nandini2114001@akgec.ac.in>
2022-02-14 21:29:50 +0530

    Update InputOutputInVectorsOfVectors.cpp.cpp

--- Array/vectors/InputOutputInVectorsOfVectors.cpp.cpp
+++ Array/vectors/InputOutputInVectorsOfVectors.cpp.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(){
     vector<vector<int>> vec;
     int rows,column,x;
+    cout<<"enter rows"<<endl;
     cin>>rows;
     //input
     for(int i=0;i<rows;i++){
         vector<int> temp;
+        cout<<"enter column"<<endl;
         cin>>column;
         for(int j=0;j<column;j++){
             cin>>x;
@@ -16,6 +18,7 @@ int main(){
         vec.push_back(temp);
     }
     //output
+    cout<<"vector:"<<endl;
     for(int i=0;i<vec.size();i++){
         for(int j=0;j<vec[i].size();j++){
             cout<<vec[i][j]<<" ";
